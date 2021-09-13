fc8890823d2597f6c4eef58f0c5cf1251628e778
dennistuk95 <88230815+dennistuk95@users.noreply.github.com>
2021-09-13 18:58:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hello Winc Academy");
+let firstName = 'Dennis';
+let x = 4;
+let y = 8;
+let z = 40;
+console.log(x + x);
+console.log(firstName);
+console.log(z / x - x);
+console.log(x * y / z);
+console.log(z % x);
+let leeftijd = 48;
+console.log(leeftijd)
+console.log(typeof leeftijd);
+leeftijd = '48';
+console.log(leeftijd)
+console.log(typeof leeftijd);
